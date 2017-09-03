25f908b7643c584066968250e01a79d5239d737c
Pedro De Ona <Pedro De Ona>
2017-09-03 00:24:23 -0400

    fix false positive specs

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let :person do
+    person = Person.new(first_name: 'Alice', last_name: 'Smith')
+  end
+
+  it 'is invalid without a first_name' do
+    person.first_name = nil
+    expect(person).not_to be_valid
+  end
+
+  it 'is invalid without a last_name' do
+    person.last_name = nil
+    expect(person).not_to be_valid
+  end
+
 end
